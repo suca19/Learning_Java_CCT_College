1844a795741cc888c8d35fa248e2f15396117c3f
Carlos <carlos.enrique.suca@gmail.com>
2026-04-20 19:08:09 +0100

    adding switch example to loops method

--- Main.java
+++ Main.java
@@ -208,6 +208,33 @@ public class Main {
             System.out.println("Iteration: " + k);
             k++;
         } while (k < 5);
-    }
     
+
+    // Example of a switch statement
+    System.out.println("\nSwitch Statement:");
+    int day = 3;
+    
+    switch (day) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        default:
+            System.out.println("Weekend");
+    
+    }  
+            
+    }
 }
+
